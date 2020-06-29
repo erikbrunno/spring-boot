8144f86e499dc9ba076e6df1f7a0fac906f34924
erikbrunno <erik.brunno@yahoo.com.br>
2020-06-28 22:52:59 -0300

    resolvendo problma de ambiguidade de beans usando uma anotacao customizada com o @Qualifier

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,18 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
 @Component
 public class AtivacaoClienteService {
 
 	@Autowired
+	@TipoDoNotificador(NivelUrgencia.URGENTE)
 	private Notificador notificador;
-	
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+
 	}
-		
+
 }
