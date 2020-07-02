d3e5be3ba7fa86f90d668a4098801f04e653c515
erikbrunno <erik.brunno@yahoo.com.br>
2020-07-02 00:39:37 -0300

    criando repository para entidade Restaurante, utilizando lombok na class Restaurante

--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,3 +1,7 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
 insert into cozinha (nome) values ('Brasileira');
+
+insert into restaurante (nome, taxa_frete) values ('Restaurante acacia', 4.00);
+insert into restaurante (nome, taxa_frete) values ('Restaurante sabor do lima', 6.00);
+insert into restaurante (nome, taxa_frete) values ('Liu da carne de sol', 5.00);
