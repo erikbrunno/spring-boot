361c2610e34ece0e97238ac4c6deb88bf386f72b
erikbrunno <erik.brunno@yahoo.com.br>
2020-07-15 00:20:33 -0300

    externalizando as consultas jpql definidas no @Query  para o arquivo orm.xml

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -14,12 +15,16 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	List<Restaurante> findByNomeAndCozinhaId(String nome, Long cozinha);
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :idCozinha")
+	List<Restaurante> consultarPorNome(String nome, @Param("idCozinha") Long cozinhaId);
+	
+//	List<Restaurante> findByNomeAndCozinhaId(String nome, Long cozinha);
 	
 	Optional<Restaurante> findFirstByNomeCointaning(String nome); 
 	
 	List<Restaurante> findTop2ByNomeContaining(String nome);
 	
 	int countByCozinhaId(Long cozinha);
+	
 }
 
