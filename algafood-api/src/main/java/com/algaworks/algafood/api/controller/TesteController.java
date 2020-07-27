460a2d3add46a1208ccb9fc5930818490ada9ef5
erikbrunno <erik.brunno@yahoo.com.br>
2020-07-27 20:25:41 -0300

    definindo um CustomJpaRepository

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,8 +1,5 @@
 package com.algaworks.algafood.api.controller;
 
-import static com.algaworks.algafood.infraestructure.repository.spec.RestauranteSpecs.comFreteGratis;
-import static com.algaworks.algafood.infraestructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
-
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @RestController
@@ -20,6 +19,9 @@ public class TesteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping("/restaurantes/por-nome-e-frete")
 	public List<Restaurante> restaurantePorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -30,6 +32,12 @@ public class TesteController {
 	public List<Restaurante> restauranteComFreteGratis(String nome) {
 		
 		return restauranteRepository.findComFreteGratis(nome);
+	}
+	
+	@GetMapping("/restaurantes/primeiro")
+	public Cozinha restaurantePrimeiro() {
+		
+		return cozinhaRepository.buscarPrimeiro().get();
 	}
 	
 
