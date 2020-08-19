47fd5a86103fdd2c5420c2eeb940610d0cf4dfb9
erikbrunno <erik.brunno@yahoo.com.br>
2020-08-18 23:03:17 -0300

    usando a especificacao rfc 7807 para definir o formato do corpo da requisicao em caso de erros na aplicacao

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,9 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -16,37 +15,73 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e,
+	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e,
 			WebRequest request) {
 
-		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-	}
-	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e, WebRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
 
-		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-	}
-	
-	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+		var problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
 		
-		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.build();
+
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
 	}
-	
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> handleNegocioException(NegocioException e, WebRequest request) {
+
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		var problemType = ProblemType.ERRO_NEGOCIO;
+		
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.build();
+		
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+	}
+
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+
+		HttpStatus status = HttpStatus.CONFLICT;
+		var problemType = ProblemType.ENTIDADE_EM_USO;
+		
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.build();
+		
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+	}
+
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
-		
+
 		if (body == null) {
-			body = Problema.builder().dataHora(LocalDateTime.now()).mensagem(status.getReasonPhrase()).build();
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
 		} else if (body instanceof String) {
-			body = Problema.builder().dataHora(LocalDateTime.now()).mensagem(ex.getMessage()).build();
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
 		}
-		
+
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		var detail = "O corpo da requisição está inválido, verifique erro de sintaxe";
+		var problem = createProblemBuilder(status, problemType, detail)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}	
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
+				.detail(detail);
+	}
+	
 }
