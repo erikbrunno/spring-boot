b32adf8dfd91b3bc8c0168c1c35d756a0b98e721
erikbrunno <erik.brunno@yahoo.com.br>
2020-08-20 00:57:26 -0300

    ajustando o tratamento de exception handler, adicionando timestamp e userMessage na classe Problema

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
@@ -23,49 +27,78 @@ import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	public static final String MSG_ERRO_SISTEMA = "Ocorreu um erro interno inesperado no sistema. "
+            + "Tente novamente e se o problema persistir, entre em contato "
+            + "com o administrador do sistema.";
+	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e,
+	public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e,
 			WebRequest request) {
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
-
-		var problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
-
-		var problem = createProblemBuilder(status, problemType, e.getMessage()).build();
+		var problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.userMessage(e.getMessage())
+				.build();
 
 		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
 	}
 
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> handleNegocioException(NegocioException e, WebRequest request) {
+	public ResponseEntity<?> handleNegocio(NegocioException e, WebRequest request) {
 
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		var problemType = ProblemType.ERRO_NEGOCIO;
-
-		var problem = createProblemBuilder(status, problemType, e.getMessage()).build();
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.userMessage(e.getMessage())
+				.build();
 
 		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
 	}
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleUncaught(Exception e,
+			WebRequest request) {
+
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		var problemType = ProblemType.ERRO_DE_SISTEMA;
+		
+	    // Importante colocar o printStackTrace (pelo menos por enquanto, que não estamos
+	    // fazendo logging) para mostrar a stacktrace no console
+	    e.printStackTrace();
+	    
+	    var problem = createProblemBuilder(status, problemType, MSG_ERRO_SISTEMA)
+	    		.userMessage(MSG_ERRO_SISTEMA)
+	    		.build();
+
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+	}
+	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+	public ResponseEntity<?> handleEntidadeEmUso(EntidadeEmUsoException e, WebRequest request) {
 
 		HttpStatus status = HttpStatus.CONFLICT;
 		var problemType = ProblemType.ENTIDADE_EM_USO;
-
-		var problem = createProblemBuilder(status, problemType, e.getMessage()).build();
+		var problem = createProblemBuilder(status, problemType, e.getMessage())
+				.userMessage(e.getMessage())
+				.build();
 
 		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
 	}
+
 
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
 
 		if (body == null) {
-			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value())
+					.userMessage(MSG_ERRO_SISTEMA)
+					.build();
 		} else if (body instanceof String) {
-			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value())
+					.userMessage(MSG_ERRO_SISTEMA)
+					.build();
 		}
 
 		return super.handleExceptionInternal(ex, body, headers, status, request);
@@ -78,29 +111,70 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Throwable rootCause = ExceptionUtils.getRootCause(ex);
 
 		if (rootCause instanceof InvalidFormatException) {
-			return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+			return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
 		} else if (rootCause instanceof PropertyBindingException) {
-			return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request);
+			return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
 		}
 
 		var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
 		var detail = "O corpo da requisição está inválido, verifique erro de sintaxe";
-		var problem = createProblemBuilder(status, problemType, detail).build();
+		var problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_SISTEMA)
+				.build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
-	private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex, HttpHeaders headers,
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		if(ex instanceof MethodArgumentTypeMismatchException) {			
+			return handleMethodArgumentTypeMismatch((MethodArgumentTypeMismatchException) ex, headers, status, request);
+		}
+		
+		return super.handleTypeMismatch(ex, headers, status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		var problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+		var detail = String.format("O recurso %s que você tentou acessar é inexistente", ex.getRequestURL());
+		var problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_SISTEMA)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		var detail = String.format(
+				"O parametro de URL '%s' recebeu o valor '%s' que é de um tipo inválido, corrija e informe um valor compatível com o tipo %s",
+				ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+		var problemType = ProblemType.PARAMETRO_INVALIDO;
+		var problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_SISTEMA)
+				.build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	private ResponseEntity<Object> handlePropertyBinding(PropertyBindingException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
 
 		var pathString = joinPath(ex.getPath());
-
 		var problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
 		String detail = String.format(
 				"A propriedade '%s' não existe. " + "Corrija ou remova essa propriedade e tente novamente.",
 				pathString);
-
-		var problem = createProblemBuilder(status, problemType, detail).build();
+		
+		var problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_SISTEMA)
+				.build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
@@ -109,7 +183,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return references.stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
 	}
 
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers,
+	private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
 
 		var pathString = joinPath(ex.getPath());
@@ -120,14 +194,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 						+ "Corrija  e informe um valor compatível com o tipo %s' ",
 				pathString, ex.getValue(), ex.getTargetType().getSimpleName());
 
-		var problem = createProblemBuilder(status, problemType, detail).build();
+		var problem = createProblemBuilder(status, problemType, detail)
+				.userMessage(MSG_ERRO_SISTEMA)
+				.build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
 		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
-				.detail(detail);
+				.detail(detail).timestamp(LocalDateTime.now());
 	}
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -4,6 +4,8 @@
 */
 package com.algaworks.algafood.api.exceptionhandler;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -19,4 +21,7 @@ public class Problem {
 	private String type;
 	private String title;
 	private String detail;
+	
+	private String userMessage;
+	private LocalDateTime timestamp;
 }
