108624785b3dc3b6f201a83ea90da92a08353611
erikbrunno <erik.brunno@yahoo.com.br>
2020-09-23 01:50:58 -0300

    criando classes servico, respositoy, model, input e assemblers para entidade usuario

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.UsuarioNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
+
+@Service
+public class CadastroUsuarioService {
+
+	private static final String MSG_USUARIO_EM_USO = "Grupo de código %d não pode ser removida, pois está em uso";
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public Usuario salvar(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	public void delete(Long usuarioId) {
+		try {
+			usuarioRepository.deleteById(usuarioId);
+			usuarioRepository.flush();
+		} catch (EmptyResultDataAccessException e) {
+			throw new UsuarioNaoEncontradoException(usuarioId);
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format(MSG_USUARIO_EM_USO, usuarioId));
+		}
+	}
+	
+	public Usuario buscar(Long usuarioId) {
+		return usuarioRepository.findById(usuarioId).orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
+	}
+
+}
