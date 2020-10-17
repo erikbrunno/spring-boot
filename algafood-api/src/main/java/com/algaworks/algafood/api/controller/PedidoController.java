e1080a169ca2bf32cf47d51b4bc7356f28fc68cf
erikbrunno <erik.brunno@yahoo.com.br>
2020-10-16 23:37:56 -0300

    comentando codigo para utilizando de JsonFilter, mais deixando guardado para historico

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -51,30 +51,30 @@ public class PedidoController {
     @Autowired
     private PedidoInputDisassembler pedidoInputDisassembler;
     
-	@GetMapping
-	public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
-		List<Pedido> todosPedidos = pedidoRepository.findAll();
-		List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
-
-		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
-
-		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
-
-		if (StringUtils.isNotBlank(campos)) {
-			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
-		}
-		
-		pedidosWrapper.setFilters(filterProvider);
-
-		return pedidosWrapper;
-	}
+//	@GetMapping
+//	public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+//		List<Pedido> todosPedidos = pedidoRepository.findAll();
+//		List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+//
+//		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+//
+//		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+//		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+//
+//		if (StringUtils.isNotBlank(campos)) {
+//			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+//		}
+//		
+//		pedidosWrapper.setFilters(filterProvider);
+//
+//		return pedidosWrapper;
+//	}
     
-//    @GetMapping
-//    public List<PedidoResumoModel> listar() {
-//        List<Pedido> todosPedidos = pedidoRepository.findAll();
-//        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
-//    }
+    @GetMapping
+    public List<PedidoResumoModel> listar() {
+        List<Pedido> todosPedidos = pedidoRepository.findAll();
+        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+    }
     
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscar(@PathVariable String codigoPedido) {
