ce7f6b718f1330b3474a2f9b495da1af24336323
erikbrunno <erik.brunno@yahoo.com.br>
2020-10-27 02:05:27 -0300

    implementando endpoint para upload de arquivos e criando anotacao para definir o tamnho maximo do arquivo permitido

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.api.model.input;
+
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.core.validation.FileSize;
+import com.sun.istack.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	@NotNull
+	@FileSize(max = "1KB")
+	private MultipartFile arquivo;
+	
+	@NotBlank
+	private String descricao;
+	
+	
+}
