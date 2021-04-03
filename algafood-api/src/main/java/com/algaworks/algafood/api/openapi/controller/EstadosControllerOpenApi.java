e7975365ff21b91ed51cfb69f9d7e20ba2cfa4e7
erikbrunno <erik.brunno@yahoo.com.br>
2021-04-03 15:35:35 -0300

    criando ajuste na documentacao da api para estados

--- algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/EstadosControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/EstadosControllerOpenApi.java
@@ -2,12 +2,15 @@ package com.algaworks.algafood.api.openapi.controller;
 
 import java.util.List;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.EstadoModel;
 import com.algaworks.algafood.api.model.input.EstadoInput;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Estados")
 public interface EstadosControllerOpenApi {
@@ -15,6 +18,9 @@ public interface EstadosControllerOpenApi {
 	@ApiOperation("Lista de Estados")
 	public List<EstadoModel> listar();
 	
+	@ApiResponses({ 
+		@ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class) })
 	@ApiOperation("Busca um Estado por ID")
 	public EstadoModel buscar(@ApiParam(value = "ID de um Estado", example = "1", required = true) Long estadoId);
 	
