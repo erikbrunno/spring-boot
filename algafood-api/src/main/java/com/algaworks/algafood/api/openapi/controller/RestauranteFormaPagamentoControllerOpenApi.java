dba330d043439011306554c0842cd3136012f4ea
erikbrunno <erik.brunno@yahoo.com.br>
2021-04-03 19:34:24 -0300

    ajustar documentacao da api para formas de pagamento

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.model.FormaPagamentoModel;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteFormaPagamentoControllerOpenApi {
+
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Restaurante não encontrado")
+	})
+	@ApiOperation("Lista as formas de pagamento de um restaurante")
+	public List<FormaPagamentoModel> listar(@ApiParam(value="ID do restaurante", example = "1") Long restauranteId);
+	
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+		@ApiResponse(code = 404, message = "Forma de pagamento ou restaurante nao encontrado")
+		
+	})
+	@ApiOperation("Desassocia uma forma de pagamento de um restaurante")
+	public void desassociar(
+			@ApiParam(value="ID do restaurante", example = "1")
+			Long restauranteId, 
+			
+			@ApiParam(value="ID da forma de pagamento", example = "1")
+			Long formaPagamentoId);
+	
+	
+
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+		@ApiResponse(code = 404, message = "Forma de pagamento ou restaurante nao encontrado")
+		
+	})
+	@ApiOperation("Associa uma forma de pagamento de um restaurante")
+	public void associar(
+			@ApiParam(value="ID do restaurante", example = "1")
+			Long restauranteId,
+			
+			@ApiParam(value="ID da forma de pagamento", example = "1")
+			Long formaPagamentoId);	
+}
