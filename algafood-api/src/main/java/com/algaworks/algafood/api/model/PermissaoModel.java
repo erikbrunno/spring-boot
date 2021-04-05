5db5b8a03364c328151c15a04a5bd5b9d6bb0186
erikbrunno <erik.brunno@yahoo.com.br>
2021-04-05 01:06:48 -0300

    ajustar documentacao da api para GrupoPermissaoController

--- algafood-api/src/main/java/com/algaworks/algafood/api/model/PermissaoModel.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/PermissaoModel.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,12 @@ import lombok.Setter;
 @Setter
 public class PermissaoModel {
 	
+	@ApiModelProperty(value = "Id da permissao", example = "1")
 	private Long id;
+	
+	@ApiModelProperty(value = "Nome da permissao", example = "Alterar")
 	private String nome;
+	
+	@ApiModelProperty(value = "Descrição da permissao", example = "Pode alterar qualquer coisa")
 	private String descricao;
 }
