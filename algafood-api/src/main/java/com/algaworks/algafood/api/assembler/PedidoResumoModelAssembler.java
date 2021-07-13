a9f134528e859bc82a792ee5fca3c7a406345fd3
erikbrunno <erik.brunno@yahoo.com.br>
2021-07-13 09:40:37 -0300

    add link no recurso de pedido

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoModelAssembler.java
@@ -1,11 +1,16 @@
 package com.algaworks.algafood.api.assembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.controller.PedidoController;
+import com.algaworks.algafood.api.controller.RestauranteController;
+import com.algaworks.algafood.api.controller.UsuarioController;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 
@@ -22,6 +27,11 @@ public class PedidoResumoModelAssembler extends RepresentationModelAssemblerSupp
     public PedidoResumoModel toModel(Pedido pedido) {
         PedidoResumoModel pedidoResumoModel = createModelWithId(pedido.getId(), pedido);
         modelMapper.map(pedido, pedidoResumoModel);
+        
+        pedidoResumoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class).buscar(pedido.getId()))
+        		.withSelfRel());
+        pedidoResumoModel.getCliente().add(linkTo(methodOn(UsuarioController.class).buscar(pedido.getCliente().getId()))
+        		.withSelfRel());
         
         return pedidoResumoModel;
     }
