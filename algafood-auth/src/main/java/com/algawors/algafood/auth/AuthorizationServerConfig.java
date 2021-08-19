72709c996bd4aea6161578d6e7a9e3447aa8973b
erikbrunno <erik.brunno@yahoo.com.br>
2021-08-19 18:58:07 -0300

    configurando authorization server

--- algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
@@ -1,11 +1,45 @@
 package com.algawors.algafood.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+	@Autowired
+	private PasswordEncoder passwordEnconder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+				.withClient("algafood-web")
+				.secret(passwordEnconder.encode("web123"))
+				.authorizedGrantTypes("password")
+				.scopes("write", "read")
+				.accessTokenValiditySeconds(60 * 60 * 6); //Expira em 6 horas
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("isAuthenticated()");
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
 }
+
+
