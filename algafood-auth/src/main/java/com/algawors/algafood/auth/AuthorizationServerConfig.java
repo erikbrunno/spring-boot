cb7956d0752d2c105bcf7b1bbbc1a3f20125adce
erikbrunno <erik.brunno@yahoo.com.br>
2021-10-09 13:41:05 -0300

    Configurando uso do refresh_token no fluxo password credentials

--- algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algawors.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
 			.inMemory()
 				.withClient("algafood-web")
 				.secret(passwordEnconder.encode("web123"))
-				.authorizedGrantTypes("password")
+				.authorizedGrantTypes("password", "refresh_token")
 				.scopes("write", "read")
-				.accessTokenValiditySeconds(60 * 60 * 6); //Expira em 6 horas
+				.accessTokenValiditySeconds(6 * 60 * 60) //Expira em 6 horas o access token
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60); //Expira em 60 dias o refresh token 
 	}
 	
 	@Override
@@ -43,7 +48,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	 */
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false); //Desabilita o reudo do refresh token
 	}
 }
 
