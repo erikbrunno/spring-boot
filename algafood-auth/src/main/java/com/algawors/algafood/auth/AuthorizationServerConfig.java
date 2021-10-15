365a6d5a5f36bd8672803fae1a0b3422fde5cf83
erikbrunno <erik.brunno@yahoo.com.br>
2021-10-15 18:53:37 -0300

    alterar secret key no authorization server

--- algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algawors/algafood/auth/AuthorizationServerConfig.java
@@ -20,8 +20,6 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-	private static final String SECRET_KEY = "algawork_s4r56eqa25d2_curso_5d8e2x59d5asdf5";
 	
 	@Autowired
 	private PasswordEncoder passwordEnconder;
@@ -84,8 +82,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey(SECRET_KEY); //Chave secreta
-	
+		jwtAccessTokenConverter.setSigningKey("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s");
+		
 		return jwtAccessTokenConverter;
 	}
 	
