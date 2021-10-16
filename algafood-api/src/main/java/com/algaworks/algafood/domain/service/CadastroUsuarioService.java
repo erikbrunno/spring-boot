86304008015368c0dc9099127f04c5fcc68474fc
erikbrunno <erik.brunno@yahoo.com.br>
2021-10-16 19:25:26 -0300

    add Crypt para gerar senha criptografada a hora de criar/alterar usuario

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +27,9 @@ public class CadastroUsuarioService {
 	@Autowired
 	private CadastroGrupoService cadastroGrupo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
 		
@@ -37,18 +41,22 @@ public class CadastroUsuarioService {
 			throw new NegocioException(String.format("Já existe um usuário cadastrado com email %s", usuario.getEmail()));
 		}
 
+		if (usuario.isNovo()) {
+			usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		}
+		
 		return usuarioRepository.save(usuario);
 	}
 	
 	@Transactional
 	public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
-		Usuario usuario = buscar(usuarioId);
-
-		if (usuario.senhaNaoCoincideCom(senhaAtual)) {
-			throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
-		}
-
-		usuario.setSenha(novaSenha);
+	    Usuario usuario = buscar(usuarioId);
+	    
+	    if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
+	        throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
+	    }
+	    
+	    usuario.setSenha(passwordEncoder.encode(novaSenha));
 	}
 	
 	public void delete(Long usuarioId) {
