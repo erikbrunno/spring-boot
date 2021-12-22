6a44b850b7a40daff9e1276263ade4a8cb21feca
erikbrunno <erik.brunno@yahoo.com.br>
2021-12-22 20:46:41 -0300

    fixbug no fluxo Client Credentials

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -48,14 +48,14 @@ public @interface CheckSecurity {
 	   
 		@PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
 		@PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or "
-				+ "@algaSecurity.getUsuarioId() == returnObject.cliente.id or "
+				+ "@algaSecurity.usuarioAutenticadoIgual(returnObject.cliente.id) or "
 				+ "@algaSecurity.gerenciaRestaurante(returnObject.restaurante.id)")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
 	    public @interface PodeBuscar { }
 		
 		@PreAuthorize("hasAuthority('SCOPE_READ') and (hasAuthority('CONSULTAR_PEDIDOS') or " 
-				+ "@algaSecurity.getUsuarioId() == #filter.clienteId or"
+				+ "@algaSecurity.usuarioAutenticadoIgual(#filter.clienteId) or"
 				+ "@algaSecurity.gerenciaRestaurante(#filter.restauranteId))")
 		@Retention(RUNTIME)
 		@Target(METHOD)
@@ -119,13 +119,13 @@ public @interface CheckSecurity {
 	public @interface UsuariosGruposPermissoes {
 
 	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and "
-	            + "@algaSecurity.getUsuarioId() == #usuarioId")
+	            + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId)")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
 	    public @interface PodeAlterarPropriaSenha { }
 	    
 	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and (hasAuthority('EDITAR_USUARIOS_GRUPOS_PERMISSOES') or "
-	            + "@algaSecurity.getUsuarioId() == #usuarioId)")
+	            + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId))")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
 	    public @interface PodeAlterarUsuario { }
