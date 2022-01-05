798998f493dbba79eccc9656ecd2363817e250c5
erikbrunno <erik.brunno@yahoo.com.br>
2022-01-04 22:50:38 -0300

    juntando o authorization server no mesmo projeto do resource server

--- algafood-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -2,8 +2,6 @@ package com.algaworks.algafood.core.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,17 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Autowired
 	private ApiRetirementHandler apiDeprecatedHandler;
-	
-	/*
-	 * Habilita o CORS para qualquer origem
-	 */
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedMethods("*");
-//			.maxAge(30) Ativa o prefligth para 30 segundos
-	}
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
