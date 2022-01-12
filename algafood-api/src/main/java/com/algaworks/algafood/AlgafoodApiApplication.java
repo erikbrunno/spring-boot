8576fca7217acab3591dadb37ecab537374d2806
erikbrunno <erik.brunno@yahoo.com.br>
2022-01-12 11:31:16 -0300

    criando Base64ProtocolResolver para transformar o base64 do arquivo JKS(Java Key Store) em um Resource

--- algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.algaworks.algafood.core.io.Base64ProtocolResolver;
 import com.algaworks.algafood.infraestructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
@@ -16,7 +17,12 @@ public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone(UTC));
-		SpringApplication.run(AlgafoodApiApplication.class, args);
+		
+		var app = new SpringApplication(AlgafoodApiApplication.class);
+		app.addListeners(new Base64ProtocolResolver());
+		app.run(args);
+		
+//		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
 
 }
